3348ef6ca6143875b9a8aa54c89b5b9ac4423903
asinghani <anish_oss@anishsinghani.com>
2018-07-28 19:47:03 -0700

    Vendored Pathfinder library (from https://github.com/JacisNonsense/Pathfinder)

---
+++ pathfinder/include/pathfinder.h
@@ -0,0 +1,27 @@
+#ifndef PATHFINDER_H_DEF
+#define PATHFINDER_H_DEF
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "pathfinder/mathutil.h"
+#include "pathfinder/structs.h"
+
+#include "pathfinder/fit.h"
+#include "pathfinder/spline.h"
+#include "pathfinder/trajectory.h"
+
+#include "pathfinder/modifiers/tank.h"
+#include "pathfinder/modifiers/swerve.h"
+
+#include "pathfinder/followers/encoder.h"
+#include "pathfinder/followers/distance.h"
+
+#include "pathfinder/io.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
